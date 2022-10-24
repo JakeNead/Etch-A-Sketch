a5263f37a3d290eb46189d23afbe3fd31891d134
Jake Nead <jakenead@gmail.com>
2022-10-24 10:58:34 -0700

    Create resize prompt that handles correct and incorrect user input

--- eas.js
+++ eas.js
@@ -1,7 +1,6 @@
 const gridContainer = document.querySelector('#gridContainer')
-
-const num = 10
-for (let i = 0; i < 256; i++) {
+let divNumber = 256;
+for (let i = 0; i < (divNumber); i++) {
     const div = document.createElement('div')
     div.classList.add('pixel')
     gridContainer.appendChild(div);
@@ -14,4 +13,21 @@ pixel.forEach((pixel) => {
   })
 })
 
-// add grid size change buttons
+function selectGridSize () {
+  gridSize = prompt ('Please enter a size between 10 and 100')
+    if (gridSize === null) {
+      return
+    } else if (gridSize < 10 || gridSize > 100 || isNaN(gridSize)) {
+      alert ('Try again!')
+      selectGridSize()
+    } 
+    divNumber = (gridSize ** 2)
+}
+
+const resize = document.getElementById('resize')
+resize.addEventListener('click', () => {
+  selectGridSize()
+})
+
+
+//next steps: find out how to replace the drawing grid when user enters new number
